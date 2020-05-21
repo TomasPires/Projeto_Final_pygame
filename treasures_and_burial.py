6a91193bf347e247c88873459299f1d65a2d3ab5
Tomas Pires <tomassp@al.insper.edu.br>
2020-05-21 15:01:46 -0300

    adicionando mais imagens

--- treasures_and_burial.py
+++ treasures_and_burial.py
@@ -1,22 +1,36 @@
 import pygame
 import random
-
-pygame.init()
+import os
 
 WIDTH = 1080
 HEIGHT = 720
+FPS = 30
 
+ #Iniciando o PyGame, algumas funções e criando a janela
+pygame.init()
+pygame.mixer.init()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Treasures and Burial')
+clock = pygame.time.Clock()
 
+#class Character(pygame.sprite.Sprite):
+ #   def __init__(self,image):
+
+all_sprites = pygame.sprite.Group() 
+#Loop principal
 gamerun = True
 
 while gamerun:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gamerun = False
     
-    window.fill((0,0,0))
+    all_sprites.update()
+
+    window.fill((0,0,0)) #Depois, podemos usar o comando pygame.display.flip()
+    all_sprites.draw(window)
+
     pygame.display.update()
 
 pygame.quit()
