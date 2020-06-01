575ff765355c1048901dc16727779dcc681fe73d
Tomas Pires <tomassp@al.insper.edu.br>
2020-06-01 18:03:54 -0300

    Atualizando o dicionário assets e construindo a mecânica de tiro

--- treasures_and_burial.py
+++ treasures_and_burial.py
@@ -1,9 +1,9 @@
 import pygame
 import random
 import os
-
-WIDTH = 1080
-HEIGHT = 720
+from math import *
+WIDTH = 600
+HEIGHT = 500
 FPS = 30
 
 WHITE = (255,255,255)
@@ -27,6 +27,49 @@ def load_assets():
     assets = {}
     assets['flecha_img'] = pygame.image.load('Pixel_TreasuresandBurial/props/objects/projectiles/arrow-16x16.png').convert_alpha()
     assets['character_img'] = pygame.image.load('Pixel_TreasuresandBurial/props/characters/front/char0.0-96x96.png').convert_alpha()
+    anim_right = []
+    anim_left = []
+    anim_front = []
+    anim_back = []
+    for i in range(10):
+        frame_right = 'Pixel_TreasuresandBurial/props/characters/right/char1.{0}-96x96.png'.format(i)
+        img = pygame.image.load(frame_right).convert_alpha()
+        img = pygame.transform.scale(img,(96,96))
+        anim_right.append(img)
+    assets['character_right'] = anim_right
+    for i in range(10):
+        frame_left = 'Pixel_TreasuresandBurial/props/characters/left/char3.{0}-96x96.png'.format(i)
+        img = pygame.image.load(frame_left).convert_alpha()
+        img = pygame.transform.scale(img,(96,96))
+        anim_left.append(img)
+    assets['character_left'] = anim_left
+    for i in range(8):
+        frame_front = 'Pixel_TreasuresandBurial/props/characters/front/char0.{0}-96x96.png'.format(i)
+        img = pygame.image.load(frame_front).convert_alpha()
+        img = pygame.transform.scale(img,(96,96))
+        anim_front.append(img)
+    assets['character_front'] = anim_front
+    for i in range(8):
+        frame_back = 'Pixel_TreasuresandBurial/props/characters/back/char2.{0}-96x96.png'.format(i)
+        img = pygame.image.load(frame_back).convert_alpha()
+        img = pygame.transform.scale(img,(96,96))
+        anim_front.append(img)
+    assets['character_back'] = anim_back
+    anim_torch = []
+    for i in range(1,5):
+        frame_torch = 'Pixel_TreasuresandBurial/props/animation/fire{0}-128x128.png'.format(i)
+        img = pygame.image.load(frame_torch).convert_alpha()
+        img = pygame.transform.scale(img,(32,32))
+        anim_torch.append(img)
+    assets['torch_anim'] = anim_torch 
+    elementals = []
+    for i in range(4):
+        elements = ['fire','water','air','earth']
+        filename = 'Pixel_TreasuresandBurial/props/enemies/{0}_elemental.png'.format(elements[i])
+        img = pygame.image.load(filename).convert_alpha()
+        img = pygame.transform.scale(img,(32,32))
+        elementals.append(img)
+    assets['elementals'] = elementals
     return assets
     
 #Classes
@@ -75,9 +118,9 @@ class Char(pygame.sprite.Sprite):
             # Marca o tick da nova imagem.
             self.last_shot = now
             # A nova bala vai ser criada logo acima e no centro horizontal da nave
-            new_bullet = Flecha(self.assets, self.rect.centery, self.rect.centerx)
-            self.groups['all_sprites'].add(new_bullet)
-            self.groups['all_flechas'].add(new_bullet)
+            nova_flecha = Flecha(self.assets, self.rect.centery, self.rect.centerx)
+            self.groups['all_sprites'].add(nova_flecha)
+            self.groups['all_flechas'].add(nova_flecha)
 
 class Flecha(pygame.sprite.Sprite):
     def __init__(self,assets,bottom,centerx):
@@ -88,12 +131,19 @@ class Flecha(pygame.sprite.Sprite):
         self.rect=self.image.get_rect()
         self.rect.centerx = centerx
         self.rect.bottom = bottom
-        self.speedy = -10 
+        self.speed = 10
+        mouse = list(pygame.mouse.get_pos()) 
+        distx = mouse[0] - self.rect.centerx
+        disty = mouse[1] - self.rect.centery
+        angle = atan2(disty,distx)
+        self.angle = degrees(angle)
+        self.speedx = cos(angle)*self.speed
+        self.speedy = sin(angle)*self.speed 
 
     def update(self):
         # A bala só se move no eixo y
         self.rect.y += self.speedy
-
+        self.rect.x += self.speedx
         # Se o tiro passar do inicio da tela, morre.
         if self.rect.bottom < 0:
             self.kill()
@@ -119,26 +169,30 @@ def janela(window):
             if event.type == pygame.QUIT:
                 gamerun = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_a:
                     player.x_speed -=5
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_d:
                     player.x_speed +=5
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_w:
                     player.y_speed -=5
-                if event.key == pygame.K_DOWN:
+                if event.key == pygame.K_s:
                     player.y_speed +=5
                 if event.key == pygame.K_SPACE:
                     player.shoot()
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_a:
                     player.x_speed +=5
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_d:
                     player.x_speed -=5
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_w:
                     player.y_speed +=5
-                if event.key == pygame.K_DOWN:
+                if event.key == pygame.K_s:
                     player.y_speed -=5
-        
+                
+            if event.type == pygame.MOUSEMOTION:
+                mouse = list(pygame.mouse.get_pos())  ###
+                print(mouse) 
+
         if player.rect.right >= (WIDTH)-100:
             if (HEIGHT/2)-50<player.rect.bottom<(HEIGHT/2+50):
                 MAPA = BLACK
@@ -148,9 +202,9 @@ def janela(window):
         all_sprites.update()
     
         window.fill(MAPA) #Depois, podemos usar o comando pygame.display.flip()
-        pygame.draw.circle(window, BLUE,(980,360),10)
         all_sprites.draw(window)
-
+        all_flechas.draw(window)
+        pygame.draw.circle(window, BLUE,(400,360),10) ###
         pygame.display.update()
         
 janela(window)
