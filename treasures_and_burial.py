a4dc77b581f460bde7814de1a9469d75e3082b2b
Tomas Pires <tomassp@al.insper.edu.br>
2020-06-11 13:31:55 -0300

    Mudando nome das máscaras

--- treasures_and_burial.py
+++ treasures_and_burial.py
@@ -29,8 +29,33 @@ clock = pygame.time.Clock()
 
 def load_assets():
     assets = {}
-    assets['flecha_img'] = pygame.image.load('Pixel_TreasuresandBurial/props/objects/projectiles/arrow-16x16.png').convert_alpha()
+    assets['arrow_img'] = pygame.image.load('Pixel_TreasuresandBurial/props/objects/projectiles/arrow-16x16.png').convert_alpha()
     assets['character_img'] = pygame.image.load('Pixel_TreasuresandBurial/props/characters/front/char0.0-96x96.png').convert_alpha()
+    char_front = []
+    for i in range(0,8):
+        filename = 'Pixel_TreasuresandBurial/props/characters/front/char0.{0}-96x96.png'.format(i)
+        img = pygame.img.load(filename).convert_alpha()
+        img = pygame.transform.scale(img,(CHAR_SIZE, CHAR_SIZE))
+        char_front.append(img)
+    char_right = []
+    for i in range(0,10):
+        filename = 'Pixel_TreasuresandBurial/props/characters/right/char1.{0}-96x96.png'.format(i)
+        img = pygame.img.load(filename).convert_alpha()
+        img = pygame.transform.scale(img,(CHAR_SIZE, CHAR_SIZE))
+        char_right.append(img)
+    char_back = []
+    for i in range(0,8):
+        filename = 'Pixel_TreasuresandBurial/props/characters/back/char2.{0}-96x96.png'.format(i)
+        img = pygame.img.load(filename).convert_alpha()
+        img = pygame.transform.scale(img,(CHAR_SIZE, CHAR_SIZE))
+        char_back.append(img) 
+    char_left = []
+    for i in range(0,10):
+        filename = 'Pixel_TreasuresandBurial/props/characters/left/char3.{0}-96x96.png'.format(i)
+        img = pygame.img.load(filename).convert_alpha()
+        img = pygame.transform.scale(img,(CHAR_SIZE, CHAR_SIZE))
+        char_left.append(img)
+    assets['char_front'] = char_front
     assets['init_screen'] = pygame.image.load('Pixel_TreasuresandBurial/img/introscreen-500x400.png').convert()
     assets['init_screen'] = pygame.transform.scale(assets['init_screen'], (WIDTH,HEIGHT))
     over_anim = []
@@ -83,27 +108,36 @@ def load_assets():
         img = pygame.transform.scale(img,(ENEMY_SIZE,ENEMY_SIZE))
         elementals.append(img)
     assets['elementals'] = elementals
-    mapas = dict()
-    for i in range(1,8):
+    maps = dict()
+    for i in range(1,7):
         filename = 'Pixel_TreasuresandBurial/maps/Mapa{0}.1.png'.format(i)
         img = pygame.image.load(filename).convert()
-        key = 'mapa{0}.1'.format(i)
-        mapas[key] = img
-    mapas['mapa3.2'] = pygame.image.load('Pixel_TreasuresandBurial/maps/Mapa3.2.png').convert()
-    mapas['mapa4.2'] = pygame.image.load('Pixel_TreasuresandBurial/maps/Mapa4.2.png').convert()
-    mapas['mapa4.3'] = pygame.image.load('Pixel_TreasuresandBurial/maps/Mapa4.3.png').convert()
-    mapas['mapa6.2'] = pygame.image.load('Pixel_TreasuresandBurial/maps/Mapa6.2.png').convert()
-    assets['mapas'] = mapas
+        key = 'map{0}.1'.format(i)
+        maps[key] = img
+    maps['map3.2'] = pygame.image.load('Pixel_TreasuresandBurial/maps/Mapa3.2.png').convert()
+    maps['map3.0'] = pygame.image.load('Pixel_TreasuresandBurial/maps/Mapa3.0.png').convert()
+    maps['map5.0'] = pygame.image.load('Pixel_TreasuresandBurial/maps/Mapa5.0.png').convert()
+    maps['map5.2'] = pygame.image.load('Pixel_TreasuresandBurial/maps/Mapa5.2.png').convert()
+    assets['maps'] = maps
     masks = dict()
-    for i in range(2,8):
+    for i in range(2,4):
         filename = 'Pixel_TreasuresandBurial/mask/Mask{0}.1.png'.format(i)
         img = pygame.image.load(filename).convert()
         img.set_colorkey(BLACK)
-        key = 'mapa{0}.1'.format(i)
+        key = 'map{0}.1'.format(i)
         masks[key] = img
-    masks['mapa3.2'] = pygame.image.load('Pixel_TreasuresandBurial/mask/Mask3.2.png').convert()
-    masks['mapa4.2'] = pygame.image.load('Pixel_TreasuresandBurial/mask/Mask4.2.png').convert()
-    masks['mapa6.2'] = pygame.image.load('Pixel_TreasuresandBurial/mask/Mask6.2.png').convert()
+    for i in range(5,7):
+        filename = 'Pixel_TreasuresandBurial/mask/Mask{0}.1.png'.format(i)
+        img = pygame.image.load(filename).convert()
+        img.set_colorkey(BLACK)
+        key = 'map{0}.1'.format(i)
+        masks[key] = img
+    masks['map2.2'] = pygame.image.load('Pixel_TreasuresandBurial/mask/Mask2.2.png').convert()
+    masks['map3.0'] = pygame.image.load('Pixel_TreasuresandBurial/mask/Mask3.0.png').convert()
+    masks['map5.0'] = pygame.image.load('Pixel_TreasuresandBurial/mask/Mask5.0.png').convert()
+    masks['map5.2'] = pygame.image.load('Pixel_TreasuresandBurial/mask/Mask5.2.png').convert()
+    masks['map6.1'] = pygame.image.load('Pixel_TreasuresandBurial/mask/Mask6.1.png').convert()
+    
     assets['masks'] = masks
     chests = dict()
     closed_chests = []
@@ -145,8 +179,8 @@ class Char(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.centerx = WIDTH/2
         self.rect.centery = HEIGHT/2
-        self.delta = {"esquerda":0,"direita":0,"acima":0,"abaixo":0}
-        self.velo = 5
+        self.delta = {"left":0,"right":0,"up":0,"down":0}
+        self.speed = 5
         self.groups = groups
         self.assets = assets
 
@@ -154,8 +188,8 @@ class Char(pygame.sprite.Sprite):
         self.shoot_ticks = 500
 
     def update(self):
-        self.rect.x += (self.delta["direita"]-self.delta["esquerda"])*self.velo
-        self.rect.y += (self.delta["abaixo"]-self.delta["acima"])*self.velo
+        self.rect.x += (self.delta["right"]-self.delta["left"])*self.speed
+        self.rect.y += (self.delta["down"]-self.delta["up"])*self.speed
 
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
@@ -166,24 +200,26 @@ class Char(pygame.sprite.Sprite):
         if self.rect.bottom > HEIGHT:
             self.rect.bottom = HEIGHT
 
+        
+
     def shoot(self):
         now = pygame.time.get_ticks()
         elapsed_ticks = now - self.last_shot
         if elapsed_ticks > self.shoot_ticks:
             self.last_shot = now
-            nova_flecha = Flecha(self.assets, self.rect.centery, self.rect.centerx)
-            self.groups['all_sprites'].add(nova_flecha)
-            self.groups['all_flechas'].add(nova_flecha)
+            new_arrow = Arrow(self.assets, self.rect.centery, self.rect.centerx)
+            self.groups['all_sprites'].add(new_arrow)
+            self.groups['all_arrows'].add(new_arrow)
 
-    def desfazer(self):
-        self.rect.x += (self.delta["esquerda"]-self.delta["direita"])*self.velo
-        self.rect.y += (self.delta["acima"]-self.delta["abaixo"])*self.velo
+    def undo(self):
+        self.rect.x += (self.delta["left"]-self.delta["right"])*self.speed
+        self.rect.y += (self.delta["up"]-self.delta["down"])*self.speed
 
-class Flecha(pygame.sprite.Sprite):
+class Arrow(pygame.sprite.Sprite):
     def __init__(self,assets,bottom,centerx):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = assets['flecha_img']
+        self.image = assets['arrow_img']
         self.mask = pygame.mask.from_surface(self.image)
         self.rect=self.image.get_rect()
         self.rect.centerx = centerx
@@ -247,33 +283,70 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.bottom = HEIGHT
             self.speed = 0
 
+class Map(pygame.sprite.Sprite):
+    def __init__(self,assets,key):
+        pygame.sprite.Sprite.__init__(self)
+        self.key = key
+        self.image = assets['maps'][self.key]
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH/2
+        self.rect.centery = HEIGHT/2 
+
 class MapMask(pygame.sprite.Sprite):
     def __init__(self,img): 
         pygame.sprite.Sprite.__init__(self)
         self.image = img
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
-    
-def janela(window):        
+
+#Função de troca de mapa   
+def map_def(player,map_n0,map_n1,assets):
+    map_name = 'map{0}.{1}'.format(map_n0,map_n1)
+    map_key = map_name
+    map_img = assets['maps'][map_key]
+    if player.rect.top == 0 and 500 < player.rect.centerx < 600:
+        try:
+            map_n1 +=1
+            MAP = map_def(player,map_n0,map_n1,assets)
+            window.blit(MAP,(0,0))
+            pygame.display.update()
+        except:
+            map_n1 +=0
+    elif 200 < player.rect.centery < 300 and player.rect.right == WIDTH:
+        try:
+            map_n0 += 1
+            MAP = map_def(player,map_n0,map_n1,assets)
+            window.blit(MAP,(0,0))
+            pygame.display.update()
+        except:
+            map_n0 += 0
+    map_name = 'map{0}.{1}'.format(map_n0,map_n1)
+    map_key = map_name 
+    map_img = assets['maps'][map_key]
+    return map_img
+
+def game_window(window):    
+    map_n0 = 1 #primeiro digito do código do mapa
+    map_n1 = 1 #segundo digito do código do mapa  
     #grupos das sprites
     all_sprites = pygame.sprite.Group()
-    all_flechas = pygame.sprite.Group()
+    all_arrows = pygame.sprite.Group()
     all_enemies = pygame.sprite.Group()
     water_mask_group = pygame.sprite.Group()
     groups = {}
     groups['all_sprites'] = all_sprites
-    groups['all_flechas'] = all_flechas
+    groups['all_arrows'] = all_arrows
     groups['all_enemies'] = all_enemies
     assets = load_assets()
     player = Char(groups, assets)
     enemy = Enemy(groups, assets, player)
-    water_mask = MapMask(assets['masks']['mapa3.1'])
+    water_mask = MapMask(assets['masks']['map3.1'])
     all_sprites.add(player)
     all_enemies.add(enemy)
     water_mask_group.add(water_mask)
+    
     #Loop principal
     gamerun = True
-    
     while gamerun:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -281,60 +354,49 @@ def janela(window):
                 gamerun = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    player.delta["esquerda"] =1
+                    player.delta["left"] =1
                 if event.key == pygame.K_d:
-                    player.delta["direita"] =1
+                    player.delta["right"] =1
                 if event.key == pygame.K_w:
-                    player.delta["acima"] =1
+                    player.delta["up"] =1
                 if event.key == pygame.K_s:
-                    player.delta["abaixo"] =1
+                    player.delta["down"] =1
                 if event.key == pygame.K_SPACE:
                     player.shoot()
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
-                    player.delta["esquerda"] =0
+                    player.delta["left"] =0
                 if event.key == pygame.K_d:
-                    player.delta["direita"] =0
+                    player.delta["right"] =0
                 if event.key == pygame.K_w:
-                    player.delta["acima"] =0
+                    player.delta["up"] =0
                 if event.key == pygame.K_s:
-                    player.delta["abaixo"] =0
+                    player.delta["down"] =0
                 
             if event.type == pygame.MOUSEMOTION:
                 mouse = list(pygame.mouse.get_pos())
                 print(mouse) 
 
-        if player.rect.right >= (WIDTH)-100:
-            if (HEIGHT/2)-50<player.rect.bottom<(HEIGHT/2+50):
+                
 
-                MAPA = assets['mapas']['mapa3.1']
-                player.kill()
-                player = Char(groups,assets)
-                player.rect.centerx = 0
-                all_sprites.add(player)
-
-        else:
-            MAPA = assets['mapas']['mapa3.1']
-            x = 0   
-
-            
-
-        hits = pygame.sprite.groupcollide(all_enemies, all_flechas, True, True, pygame.sprite.collide_mask)
+        hits = pygame.sprite.groupcollide(all_enemies, all_arrows, True, True, pygame.sprite.collide_mask)
 
         all_sprites.update()
         
         in_water = pygame.sprite.spritecollide(player,water_mask_group,False,pygame.sprite.collide_mask)
         
         if in_water:
-            player.desfazer()
+            player.undo()
             print(in_water)
-    
-        window.blit(MAPA,(0,0)) #Depois, podemos usar o comando pygame.display.flip()
+            
+        MAP = map_def(player,map_n0,map_n1,assets)
+        window.blit(MAP,(0,0))
+        
         all_sprites.draw(window)
-        all_flechas.draw(window)
+        all_arrows.draw(window)
         all_enemies.draw(window)
         pygame.display.update()
         
-janela(window)
+game_window(window)
 
 pygame.quit()
